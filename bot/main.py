b922b86993d2487f2941fe204243b688e4c7b3ac
Arturiooo <arthur-nahushev@mail.ru>
2025-10-02 17:45:41 +0300

    Добавлен роутер, исправлены комментарии

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,22 @@
-#--- ---#
+#--- Библиотеки ---#
 import asyncio
 from aiogram import Bot, Dispatcher
 
-#--- ---#
+#--- Токен бота ---#
 from config import BOT_TOKEN
 
-#--- ---#
+#--- Ипортируем handlers ---#
 import handlers.start_handler as start_handler
+import handlers.make_order_handler as make_order_handler
 
 
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
 
-    #
+    # Объявляем роутеры
     dp.include_router(start_handler.start_router)
+    dp.include_router(make_order_handler.make_order_router)
     await dp.start_polling(bot)
 
 
