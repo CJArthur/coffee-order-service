f3b50738b4b3152e117e7efc2d707e1f0fdd03b8
Arturuio_lapt <you@example.com>
2025-10-02 10:58:36 +0300

    добавлены кнопки

--- bot/keyboards/start_message_kb.py
+++ bot/keyboards/start_message_kb.py
@@ -2,4 +2,6 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
 start_msg_kb = ReplyKeyboardMarkup(keyboard = [
-               [KeyboardButton(text = "| Сделать заказ |")] ], resize_keyboard = True)
+               [KeyboardButton(text = "📋 Сделать заказ")],
+               [KeyboardButton(text = "⚙️ Настройки")],
+               [KeyboardButton(text = "🔄 История заказов")]], resize_keyboard = True)
