199c87bdd5c43aa1d523dab3ef3a80956c192c0f
Arturiooo <arthur-nahushev@mail.ru>
2025-10-02 17:47:40 +0300

    Оживлена кнопка, добавлены состояния

---
+++ bot/handlers/make_order_handler.py
@@ -0,0 +1,25 @@
+#--- Бибилитеки ---#
+from aiogram import Router, F
+from aiogram.types import Message
+
+#--- Классы состояния ---#
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
+
+
+# Роутер
+make_order_router = Router()
+
+# Класс состояния
+class OrderSteps(StatesGroup):
+    select_main_product = State()
+
+
+@make_order_router.message(F.text == "📋 Сделать заказ")
+async def choose_product(message: Message, state = FSMContext):
+    await message.answer("ща будем тебе ебашить заказ падажи")
+
+    await state.set_state(OrderSteps.select_main_product)
+
+#@make_order_router(OrderSteps.select_main_product)
