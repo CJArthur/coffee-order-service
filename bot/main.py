2b2d3f03339b5e8241c03111e711ef99612bc416
Arturiooo <arthur-nahushev@mail.ru>
2025-10-04 17:15:14 +0300

    Добавлен роутер истории

--- bot/main.py
+++ bot/main.py
@@ -9,6 +9,7 @@ from config import BOT_TOKEN
 import handlers.start_handler as start_handler
 import handlers.make_order_handler as make_order_handler
 import handlers.settings_handler as settings_handler
+import handlers.history_handler as history_handler
 
 
 async def main():
@@ -19,6 +20,8 @@ async def main():
     dp.include_router(start_handler.start_router)
     dp.include_router(make_order_handler.make_order_router)
     dp.include_router(settings_handler.settings_router)
+    dp.include_router(history_handler.history_router)
+
     await dp.start_polling(bot)
 
 
