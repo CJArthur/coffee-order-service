b71c7245d6ded3c0f5aad71980dd05a54273fb38
sklyarvlad <sklyarvladislavtl@gmail.com>
2025-10-11 20:57:30 +0300

    init project with uv and direnv without structure of project

--- bot/keyboards/start_message_kb.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-
-
-start_msg_kb = ReplyKeyboardMarkup(keyboard = [
-               [KeyboardButton(text = "📋 Сделать заказ")],
-               [KeyboardButton(text = "⚙️ Настройки"),
-                KeyboardButton(text = "🔄 История заказов")] ], resize_keyboard = True)
